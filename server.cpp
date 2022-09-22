6ded7684106d32b189aed4a8fa7e3b05887d3e7e
Ash H <ashasaurus@protonmail.com>
2022-09-22 19:23:11 -0400

    Change to Code in Textbook

--- server.cpp
+++ server.cpp
@@ -3,44 +3,47 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <netdb.h>
- 
-#include <strings.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
 
-#define SERVER_PORT 1603
-#define MAX_PENDING 5 
-#define MAX_LINE 256
+#define SERVER_PORT  5432
+#define MAX_PENDING  5
+#define MAX_LINE     256
 
-int main(int argc, char * argv[]) {
-        struct sockaddr_in sin ;
-        char buf[MAX_LINE] ;
-        int len ;
-        int s, new_s ;
+int
+main()
+{
+  struct sockaddr_in sin;
+  char buf[MAX_LINE];
+  socklen_t buf_len, addr_len;
+  int s, new_s;
 
-        bzero((char *)&sin,sizeof(sin)) ;
-        sin.sin_family = AF_INET ;
-        sin.sin_addr.s_addr = INADDR_ANY ;
-        sin.sin_port = htons( SERVER_PORT) ; 
-        
-        if ( (s = socket(PF_INET,SOCK_STREAM,0)) < 0) {
-                perror("simplex-talk: socket") ;
-                exit(1) ;
-        }
-        if   (bind( s, (struct sockaddr *) &sin, sizeof(sin) ) < 0 ) {
-                perror("simplex-talk: bind") ; 
-                exit(1) ;
-        }
-        listen( s, MAX_PENDING ) ;
+  /* build address data structure */
+  bzero((char *)&sin, sizeof(sin));
+  sin.sin_family = AF_INET;
+  sin.sin_addr.s_addr = INADDR_ANY;
+  sin.sin_port = htons(SERVER_PORT);
 
-        while(1) {
-                if ( (new_s = accept(s,(struct sockaddr *)&sin,
-                        (socklen_t *) &len) ) < 0 ) {
-                        perror("simplex-talk: accept") ; 
-                        exit(1) ;
-                }
-                while ( (len = recv(new_s, buf, sizeof(buf), 0 )) )
-                        fputs(buf, stdout) ;
-                close(new_s) ;
-        }
+  /* setup passive open */
+  if ((s = socket(PF_INET, SOCK_STREAM, 0)) < 0) {
+    perror("simplex-talk: socket");
+    exit(1);
+  }
+  if ((bind(s, (struct sockaddr *)&sin, sizeof(sin))) < 0) {
+    perror("simplex-talk: bind");
+    exit(1);
+  }
+  listen(s, MAX_PENDING);
+
+ /* wait for connection, then receive and print text */
+  while(1) {
+    if ((new_s = accept(s, (struct sockaddr *)&sin, &addr_len)) < 0) {
+      perror("simplex-talk: accept");
+      exit(1);
+    }
+    while (buf_len = recv(new_s, buf, sizeof(buf), 0))
+      fputs(buf, stdout);
+    close(new_s);
+  }
 }
