8350c913580f5858d79f208fc63f871f91448f94
schneisp <71292451+schneisp@users.noreply.github.com>
2022-09-23 16:54:35 -0400

    Add files via upload
    Commands now work with and without spaces. NO MORE SEGMENT FAULTS for now

--- server.cpp
+++ server.cpp
@@ -6,107 +6,64 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
-#include <sqlite3.h> 
-#include <string>
 #include <iostream>
 
-#define SERVER_PORT  1879
+#define SERVER_PORT  9909
 #define MAX_PENDING  5
 #define MAX_LINE     256
 
 std::string buildCommand(char line[]) {
     std::string command = "";
-    int i = 0;
     std::cout << "entered function" << std::endl;
-    while (line[i] != ' ') {
+    size_t len = strlen(line);
+    for (size_t i = 0; i < len; i++) {
         std::cout << "looping" << std::endl;
+        if (line[i] == '\n')
+            continue;
+        if (line[i] == ' ')
+            break;
         command += line[i];
-        i++;
     }
     std::cout << "out of function loop" << std::endl;
     return command;
-
 }
 
-static int callback(void *data, int argc, char **argv, char **azColName) {
-        int i;
-        fprintf(stderr, "%s: ", (const char*)data);
-        
-        for(i = 0; i<argc; i++) {
-                printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
-        }
-        printf("\n");
-        return 0;
-}
-
-int main()
+int
+main()
 {
-        struct sockaddr_in sin;
-        char buf[MAX_LINE];
-        socklen_t buf_len, addr_len;
-        int s, new_s;
-        std::string command = "";
+  struct sockaddr_in sin;
+  char buf[MAX_LINE];
+  socklen_t buf_len, addr_len;
+  int s, new_s;
 
-        /* build address data structure */
-        bzero((char *)&sin, sizeof(sin));
-        sin.sin_family = AF_INET;
-        sin.sin_addr.s_addr = INADDR_ANY;
-        sin.sin_port = htons(SERVER_PORT);
+  std::string command = "";
 
-        /* setup passive open */
-        if ((s = socket(PF_INET, SOCK_STREAM, 0)) < 0) {
-                perror("simplex-talk: socket");
-                exit(1);
-        }
-        if ((bind(s, (struct sockaddr *)&sin, sizeof(sin))) < 0) {
-                perror("simplex-talk: bind");
-                exit(1);
-        }
-        listen(s, MAX_PENDING);
+  /* build address data structure */
+  bzero((char *)&sin, sizeof(sin));
+  sin.sin_family = AF_INET;
+  sin.sin_addr.s_addr = INADDR_ANY;
+  sin.sin_port = htons(SERVER_PORT);
 
-        /* check if at least one user is in db, if not create one*/
-        sqlite3 *db;
-        char *zErrMsg = 0;
-        int rc;
-        char *sql;
-        const char* data = "Callback function called";
+  /* setup passive open */
+  if ((s = socket(PF_INET, SOCK_STREAM, 0)) < 0) {
+    perror("simplex-talk: socket");
+    exit(1);
+  }
+  if ((bind(s, (struct sockaddr *)&sin, sizeof(sin))) < 0) {
+    perror("simplex-talk: bind");
+    exit(1);
+  }
+  listen(s, MAX_PENDING);
 
-        /* Open database */
-        rc = sqlite3_open("cis427_crypto.sqlite", &db);
-        
-        if(rc) {
-                fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
-                return(0);
-        } 
-        else {
-                fprintf(stderr, "Opened database successfully\n");
-        }
-
-        /* Create SQL statement */
-        /* Explanation:
-                if(users is empty)
-                        results in an error
-        */
-        sql = "INSERT INTO users VALUES (1, 'cis427@gmail.com', 'John', 'Smith', 'J_Smith', 'password', 100)"; // check if table is empty, i.e. no entries
-
-        /* Execute SQL statement */
-        rc = sqlite3_exec(db, sql, callback, (void*)data, &zErrMsg);
-
-        if( rc != SQLITE_OK ){
-                fprintf(stderr, "SQL error: %s\n", zErrMsg);
-                sqlite3_free(zErrMsg);
-        }
-        else {
-                fprintf(stdout, "User Added Successfully\n");
-        }
-        sqlite3_close(db);
-
-       /* wait for connection, then receive and print text */
+ /* wait for connection, then receive and print text */
   while(1) {
     if ((new_s = accept(s, (struct sockaddr *)&sin, &addr_len)) < 0) {
       perror("simplex-talk: accept");
       exit(1);
     }
+
+    char sellMessage[MAX_LINE] = "You used the Sell command";
+
     while (buf_len = recv(new_s, buf, sizeof(buf), 0)) {
         fputs(buf, stdout);
 
@@ -117,6 +74,7 @@ int main()
         }
         else if (command == "SELL") {
             std::cout << "Sell command." << std::endl;
+
         }
         else if (command == "LIST") {
             std::cout << "List command." << std::endl;
